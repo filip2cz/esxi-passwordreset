fdfd752cafe563b954c94fcaac89c2466cac3451
filip2cz <filip@fkomarek.eu>
2024-06-19 11:49:05 +0200

    ADD: disk selection

--- custom-archlive/airootfs/etc/esxi-passwordreset/script.bash
+++ custom-archlive/airootfs/etc/esxi-passwordreset/script.bash
@@ -1,1 +1,25 @@
-echo "Here will be ESXi Password Reset script"
+#!/bin/bash
+
+echo "Welcome in ESXi Password Reset iso"
+echo
+
+fdisk -l
+echo
+
+REPEAT=true
+
+while [ $REPEAT == true ]
+do
+    echo -n "Select disk with ESXi image (sdxN) [sda5]:"
+    read DISK
+
+    if [ -z "$DISK" ]; then
+        DISK="sda5"
+    fi
+    if [ ! -b "/dev/$DISK" ]; then
+        echo "invalid partition: $DISK"
+    else
+        REPEAT=false
+    fi
+done
+
