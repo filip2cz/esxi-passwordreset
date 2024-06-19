7d94d3f5da6f8f1ab9575521fb05f0e1dd6e2105
filip2cz <filip@fkomarek.eu>
2024-06-19 14:20:20 +0200

    ADD: extracting things

--- custom-archlive/airootfs/etc/esxi-passwordreset/script.bash
+++ custom-archlive/airootfs/etc/esxi-passwordreset/script.bash
@@ -23,3 +23,14 @@ do
     fi
 done
 
+mkdir /mnt/working
+mount /dev/$DISK /mnt/working
+
+mkdir /root/state
+tar -xf /mnt/working/state.tgz -C /root/state
+
+mkdir /root/localtgz
+crypto-util envelope extract --aad ESXConfiguration /root/local/local.tgz.ve /root/localtgz/local.tgz
+
+mkdir /root/local
+tar -xf /root/localtgz/local.tgz.ve -C /root/local
